71464431456eb663c126cea387f0046e5ec74788
Saurabh Kumar <117591880+saurabhsbt1@users.noreply.github.com>
2023-07-21 08:49:00 +0530

    Add files via upload

---
+++ asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for personalized_assessment_system project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personalized_assessment_system.settings')
+
+application = get_asgi_application()
